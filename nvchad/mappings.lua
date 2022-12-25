4f8f29e71cb4a19f247bf519a1599a72afc50b00
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2022-12-25 15:43:18 +0700

    feat: add mapping to close all buffer except current

--- nvchad/mappings.lua
+++ nvchad/mappings.lua
@@ -3,6 +3,8 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "command mode", opts = { nowait = true } },
+    -- close all buffers but this one
+    ["<leader>dd"] = { "<cmd> %bd|e#<cr>", "close all buffers but this one" },
   },
 }
 
