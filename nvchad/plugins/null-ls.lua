f1e2d3a9b2c3ca2b05bc139233bdb33bf4752c5b
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2023-08-29 15:09:57 +0700

    fix: remove deno_fmt

--- nvchad/plugins/null-ls.lua
+++ nvchad/plugins/null-ls.lua
@@ -9,7 +9,7 @@ local b = null_ls.builtins
 local sources = {
 
 	-- webdev stuff
-	b.formatting.deno_fmt,
+	-- b.formatting.deno_fmt,
 	b.formatting.prettier.with({
 		filetypes = {
 			"javascript",
