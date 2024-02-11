cc2aa5a4eacb1049f34bda46e553d96373969f35
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2024-02-11 21:33:59 +0700

    feat: add obsidian and lazygit plugins'

--- nvchad/plugins.lua
+++ nvchad/plugins.lua
@@ -55,7 +55,7 @@ local plugins = {
 		"nvim-tree/nvim-tree.lua",
 		opts = {
 			view = {
-				width = 20,
+				width = 40,
 				side = "right",
 				preserve_window_proportions = false,
 			},
@@ -207,6 +207,41 @@ local plugins = {
 	{
 		"hrsh7th/cmp-path",
 		enabled = false,
+	},
+	{
+		"epwalsh/obsidian.nvim",
+		version = "*", -- recommended, use latest release instead of latest commit
+		lazy = true,
+		ft = "markdown",
+		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+		-- event = {
+		--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+		--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+		--   "BufReadPre path/to/my-vault/**.md",
+		--   "BufNewFile path/to/my-vault/**.md",
+		-- },
+		dependencies = {
+			-- Required.
+			"nvim-lua/plenary.nvim",
+
+			-- see below for full list of optional dependencies 👇
+		},
+		opts = {
+			workspaces = {
+				{
+					name = "kraken",
+					path = "~/Documents/kraken",
+				},
+			},
+		},
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = false,
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
 	},
 }
 
