4b0034948132c5ffef12e5965f267d951e0fb900
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2024-07-02 14:23:12 +0800

    feat: add copilotchat

--- nvchad/plugins.lua
+++ nvchad/plugins.lua
@@ -244,6 +244,20 @@ local plugins = {
 			"nvim-lua/plenary.nvim",
 		},
 	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		lazy = false,
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		opts = {
+			debug = true, -- Enable debugging
+			-- See Configuration section for rest
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	},
 }
 
 return plugins
