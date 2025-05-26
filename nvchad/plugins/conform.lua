ce1be7c1b1e23bfa3c50eee96a0269897008fa38
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2025-05-26 14:47:24 +0800

    feat: add conform.lua plugin for code formatting

---
+++ nvchad/plugins/conform.lua
@@ -0,0 +1,26 @@
+local present, conform = pcall(require, "null-ls")
+
+if not present then
+	return
+end
+
+local config = {
+	lsp_fallback = true,
+
+	formatters_by_ft = {
+		lua = { "stylua" },
+
+		javascript = { "prettier" },
+		css = { "prettier" },
+		html = { "prettier" },
+
+		sh = { "shfmt" },
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
+}
+
+conform.setup(config)
