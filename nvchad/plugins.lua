a2dc4bb172a250a677271db8cb8e486fcee86d13
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2025-07-17 16:30:29 +0800

    feat: add lazyvim native mason

--- nvchad/plugins.lua
+++ nvchad/plugins.lua
@@ -69,7 +69,11 @@ local plugins = {
 	},
 
 	{
-		"williamboman/mason.nvim",
+		"mason-org/mason.nvim",
+    cmd = "Mason",
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+    build = ":MasonUpdate",
+    opts_extend = { "ensure_installed" },
 		opts = {
 
 			ensure_installed = {
